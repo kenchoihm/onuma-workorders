f8a355a840bbce6bf40e00a373ccaff1273fa3c0
kenchoihm <kenchoihm@yahoo.com.hk>
2014-09-30 14:00:33 +0100

    Revert "Onuma Work Orders"
    This reverts commit 38b8e56d29cc5c9142fb5cbba6cf9ed03fba4cfd.

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//define("DOMAIN", "http://192.168.0.10/");
-//define("ROOT_PATH", "C:\\htdocs\\wo\\");
-define("DOMAIN", "https://www.onuma.com/");
-define("ROOT_PATH", "/home/site5/public_html/wo/");
-
-?>
--- test.php
+++
@@ -1,3 +0,0 @@
-<input type="text" value="<?php
-echo htmlentities('xbZN9DS97fKEZ#XbY#californiaq33aS5l8jQ^%onm');
-?>" style="width:1000px;" />
